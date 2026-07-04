4b34e10bd36c1e77691da290921f2f5e46ec473f
Bianca <bianca.costamelo1@gmail.com>
2026-07-03 22:14:54 -0300

    subindo estrutura de pastas

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const btnTheme = document.getElementById("btnTheme")
+
+btnTheme.addEventListener("click", e => {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+})
